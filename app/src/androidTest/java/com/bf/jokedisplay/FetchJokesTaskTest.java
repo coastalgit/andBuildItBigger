5a2b56d2fed70d3b934f0e2b6d1f3348aaca9016
BF <bren.f.12@gmail.com>
2018-05-31 11:16:30 +0100

    Fix test and remove unused string resource entry

--- app/src/androidTest/java/com/bf/jokedisplay/FetchJokesTaskTest.java
+++ app/src/androidTest/java/com/bf/jokedisplay/FetchJokesTaskTest.java
@@ -8,15 +8,17 @@ import android.text.TextUtils;
 import com.udacity.gradle.builditbigger.FetchJokeTask;
 import com.udacity.gradle.builditbigger.IOnGetJokeListener;
 
-import org.hamcrest.text.IsEmptyString;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.*;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -26,23 +28,31 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class FetchJokesTaskTest {
 
-
     @Test
-    public void testTaskForCloud() {
+    public void testTaskForCloud() throws InterruptedException {
         Context appContext = InstrumentationRegistry.getTargetContext();
+        final CountDownLatch latch = new CountDownLatch(1);
 
         FetchJokeTask jokeTask = new FetchJokeTask(appContext, true, new IOnGetJokeListener() {
             @Override
             public void onFetchJoke_OK(String joke) {
+                latch.countDown();
                 assertThat(joke, not(isEmptyString()));
             }
 
             @Override
             public void onFetchJoke_Error(String error) {
+                latch.countDown();
                 assertThat(error, containsString("No joke"));
             }
         });
 
-        jokeTask.execute();
+        try {
+            jokeTask.execute();
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            fail();
+        }
     }
 }
